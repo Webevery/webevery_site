ca10eaf04b8c1aa89c6ab61a0bd128d52ba25d68
Webevery <webforevery@gmail.com>
2023-04-23 20:18:31 +0300

    add_routes

---
+++ src/data/navigation.data.js
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    id: Math.random(),
+    path: '/',
+    title: 'Home',
+  },
+  {
+    id: Math.random(),
+    path: '/aboutUs',
+    title: 'AboutUs',
+  },
+  {
+    id: Math.random(),
+    path: '/contacts',
+    title: 'Contacts',
+  },
+  {
+    id: Math.random(),
+    path: '/ourProjects',
+    title: 'OurProjects',
+  },
+];
