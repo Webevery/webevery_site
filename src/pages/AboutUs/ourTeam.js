77bdb40b1a68efc1cd1c0bb8692a6164a0e83c52
Nataliia <natalabojko@gmail.com>
2023-04-24 18:32:55 +0300

    Our team list

---
+++ src/pages/AboutUs/ourTeam.js
@@ -0,0 +1,73 @@
+import { nanoid } from 'nanoid';
+
+import volodymyrKavun from '../../images/ourTeam/volodymyrKavun.jpeg';
+import maksPeltec from '../../images/ourTeam/maksPeltec.png';
+import nataliiaBoiko from '../../images/ourTeam/nataliiaBoiko.jpeg';
+import ivikaDuda from '../../images/ourTeam/ivikaDuda.jpeg';
+import andriiTsapkov from '../../images/ourTeam/andriiTsapkov.jpeg';
+import dmytroColodchenko from '../../images/ourTeam/dmytroColodchenko.jpeg';
+import maksSheih from '../../images/ourTeam/maksSheih.jpeg';
+import vitaliiPavlovskyi from '../../images/ourTeam/vitaliiPavlovskyi.jpeg';
+import myroslav from '../../images/ourTeam/myroslav.jpeg';
+import defaultPhoto from '../../images/ourTeam/default.jpeg';
+
+export const ourTeam = [
+  {
+    id: nanoid(),
+    name: 'Volodymyr Kavun',
+    photo: volodymyrKavun,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Maksym Peltek',
+    photo: maksPeltec,
+  },
+  {
+    id: nanoid(),
+    name: 'Nataliia Boiko',
+    photo: nataliiaBoiko,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Ivika Duda',
+    photo: ivikaDuda,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Andrii Tsapkov',
+    photo: andriiTsapkov,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Dmytro Kolodchenko',
+    photo: dmytroColodchenko,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Maks Sheih',
+    photo: maksSheih,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Vitalii Pavlovskyi',
+    photo: vitaliiPavlovskyi,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Volodymur',
+    photo: defaultPhoto,
+  },
+
+  {
+    id: nanoid(),
+    name: 'Muroslav Kozar',
+    photo: myroslav,
+  },
+];
