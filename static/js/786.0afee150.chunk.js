1be94df347b3bfdc4f6c2f942b579ffad89346a8
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2023-04-24 21:35:03 +0000

    Deploying to gh-pages from @ Webevery/webevery_site@11d3359414bdfd5361dc9ec7af5738c407bcfa41 🚀

--- static/js/786.0afee150.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwebevery_site=self.webpackChunkwebevery_site||[]).push([[786],{786:function(e,s,t){t.r(s);t(791);var u=t(184);s.default=function(){return(0,u.jsx)("div",{children:"AboutUs"})}}}]);
-//# sourceMappingURL=786.0afee150.chunk.js.map
