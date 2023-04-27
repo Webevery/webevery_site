1c741516100fe00bb06ac303597d22dea69f2712
Nataliia <natalabojko@gmail.com>
2023-04-27 17:31:49 +0300

    added quote to ourTeam.js

--- src/components/screens/DevPage/DevPage.jsx
+++ src/components/screens/DevPage/DevPage.jsx
@@ -6,20 +6,18 @@ import { ourTeam } from '../../../data/ourTeam';
 import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-// import { Link } from 'react-router-dom';
-
 const DevPage = () => {
   const { id } = useParams();
 
   const [item, setItem] = useState(() => {
-    const initialValue = JSON.parse(localStorage.getItem('item'));
+    const initialValue = JSON.parse(localStorage.getItem('dev'));
     return initialValue || {};
   });
 
   useEffect(() => {
-    const item = ourTeam.find(item => item.id === id);
-    localStorage.setItem('item', JSON.stringify(item));
-    setItem(item);
+    const dev = ourTeam.find(item => item.id === id);
+    localStorage.setItem('dev', JSON.stringify(dev));
+    setItem(dev);
   }, [id]);
 
   return (
@@ -31,12 +29,8 @@ const DevPage = () => {
           </div>
           <h3>{item.name}</h3>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
-          tempora cumque provident? Eveniet unde quaerat quibusdam, architecto
-          facere assumenda ad pariatur neque consequuntur harum ipsa nostrum,
-          atque velit amet voluptatibus.
-        </p>
+        {/* <ImQuotesLeft /> */}
+        <p className={s.quote}>{item.quote}</p>
       </div>
       <Link className={s.goBackLinc} to="/aboutUs">
         Back to team
--- src/data/ourTeam.js
+++ src/data/ourTeam.js
@@ -15,59 +15,78 @@ export const ourTeam = [
     id: '1',
     name: 'Volodymyr Kavun',
     photo: volodymyrKavun,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '2',
     name: 'Maksym Peltek',
     photo: maksPeltec,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '3',
     name: 'Nataliia Boiko',
     photo: nataliiaBoiko,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '4',
     name: 'Ivika Duda',
     photo: ivikaDuda,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '5',
     name: 'Andrii Tsapkov',
     photo: andriiTsapkov,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '6',
     name: 'Dmytro Kolodchenko',
     photo: dmytroColodchenko,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '7',
     name: 'Maks Sheih',
     photo: maksSheih,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '8',
     name: 'Vitalii Pavlovskyi',
     photo: vitaliiPavlovskyi,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '9',
     name: 'Volodymur',
-    // photo: defaultPhoto,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 
   {
     id: '10',
     name: 'Muroslav Kozar',
     photo: myroslav,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
   },
 ];
