621d2bd0fa56cac2603853661279c925615b048f
Nataliia <natalabojko@gmail.com>
2023-04-28 16:39:36 +0300

    changed styles and fixed photo

---
+++ src/data/coworkers.data.js
@@ -0,0 +1,92 @@
+// import { nanoid } from 'nanoid';
+
+import volodymyrKavun from '../images/ourTeam/volodymyrKavun.jpeg';
+import maksPeltec from '../images/ourTeam/maksPeltec.png';
+import nataliiaBoiko from '../images/ourTeam/nataliiaBoiko.jpeg';
+import ivikaDuda from '../images/ourTeam/ivikaDuda.jpeg';
+import andriiTsapkov from '../images/ourTeam/andriiTsapkov.jpeg';
+import dmytroColodchenko from '../images/ourTeam/dmytroColodchenko.jpeg';
+import maksSheih from '../images/ourTeam/maksSheih.jpeg';
+import vitaliiPavlovskyi from '../images/ourTeam/vitaliiPavlovskyi.jpeg';
+import myroslav from '../images/ourTeam/myroslav.jpeg';
+
+export const ourTeam = [
+  {
+    id: '1',
+    name: 'Volodymyr Kavun',
+    photo: volodymyrKavun,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '2',
+    name: 'Maksym Peltek',
+    photo: maksPeltec,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '3',
+    name: 'Nataliia Boiko',
+    photo: nataliiaBoiko,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '4',
+    name: 'Ivika Duda',
+    photo: ivikaDuda,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '5',
+    name: 'Andrii Tsapkov',
+    photo: andriiTsapkov,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '6',
+    name: 'Dmytro Kolodchenko',
+    photo: dmytroColodchenko,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '7',
+    name: 'Maks Sheih',
+    photo: maksSheih,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '8',
+    name: 'Vitalii Pavlovskyi',
+    photo: vitaliiPavlovskyi,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '9',
+    name: 'Volodymur',
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+
+  {
+    id: '10',
+    name: 'Muroslav Kozar',
+    photo: myroslav,
+    quote:
+      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+  },
+];
