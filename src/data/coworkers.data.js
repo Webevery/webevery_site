069842338c8c6f91fe3ebdc55de32a1fcf2a3740
Nataliia <natalabojko@gmail.com>
2023-05-02 18:31:18 +0300

    fixed DevPage quote and Link

--- src/data/coworkers.data.js
+++ src/data/coworkers.data.js
@@ -15,16 +15,14 @@ export const ourTeam = [
     id: '1',
     name: 'Volodymyr Kavun',
     photo: volodymyrKavun,
-    quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+    quote: 'When you have a dream, you have got to grab it and never let go.',
   },
 
   {
     id: '2',
     name: 'Maksym Peltek',
     photo: maksPeltec,
-    quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+    quote: 'Nothing is impossible. The word itself says "I am possible!"',
   },
 
   {
@@ -39,16 +37,14 @@ export const ourTeam = [
     id: '4',
     name: 'Ivika Duda',
     photo: ivikaDuda,
-    quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+    quote: 'The bad news is time flies. The good news is you are the pilot.',
   },
 
   {
     id: '5',
     name: 'Andrii Tsapkov',
     photo: andriiTsapkov,
-    quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+    quote: 'There is nothing impossible to they who will try.',
   },
 
   {
@@ -56,7 +52,7 @@ export const ourTeam = [
     name: 'Dmytro Kolodchenko',
     photo: dmytroColodchenko,
     quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+      'Keep your face always toward the sunshine, and shadows will fall behind you.',
   },
 
   {
@@ -64,7 +60,7 @@ export const ourTeam = [
     name: 'Maks Sheih',
     photo: maksSheih,
     quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+      'Success is not final, failure is not fatal: it is the courage to continue that counts.',
   },
 
   {
@@ -72,14 +68,13 @@ export const ourTeam = [
     name: 'Vitalii Pavlovskyi',
     photo: vitaliiPavlovskyi,
     quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+      'You define your own life. Do not let other people write your script.',
   },
 
   {
     id: '9',
     name: 'Volodymur',
-    quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+    quote: 'You are never too old to set another goal or to dream a new dream.',
   },
 
   {
@@ -87,6 +82,6 @@ export const ourTeam = [
     name: 'Muroslav Kozar',
     photo: myroslav,
     quote:
-      'It’s worth remembering that it is often the small steps, not the giant leaps, that bring about the most lasting change.',
+      'You do not always need a plan. Sometimes you just need to breathe, trust, let go and see what happens.',
   },
 ];
