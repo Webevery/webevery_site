f514f3262a4e1c95ad0ebbcfda99bf212d535bda
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2023-05-03 15:41:42 +0300

    add modal

--- src/components/share/Modal/Modal.jsx
+++ src/components/share/Modal/Modal.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
+import css from './Modal.module.scss';
+const rootModal = document.getElementById('modal');
 
-const Modal = () => {
-    return (
-        <div>Modal</div>
-    )
+function Modal(props) {
+  useEffect(() => {
+    const keydownHandler = e => {
+      if (e.code !== 'Escape') return;
+      props.onCloseModal();
+    };
+
+    window.addEventListener('keydown', keydownHandler);
+    return () => window.removeEventListener('keydown', keydownHandler);
+  }, [props]);
+
+  function backdropClickHandler(e) {
+    if (e.currentTarget === e.target) {
+      props.onCloseModal();
+    }
+  }
+
+  const backdropMode = props.mode === 'dark' ? 'dark' : '';
+
+  return createPortal(
+    <div
+      className={`${css.backdrop} ${css[backdropMode]}`}
+      onClick={backdropClickHandler}
+    >
+      {props.children}
+    </div>,
+    rootModal
+  );
 }
 
-export default Modal
+export default Modal;
+
+// Example! How to use Modal
+
+// import { useModal } from 'hooks';
+// import Modal from 'components/share/Modal';
+
+// function Home() {
+//   const { isModalOpen, closeModal, toggleModal } = useModal();
+
+//   return (
+//     <div>
+//       {isModalOpen && (
+//         <Modal onCloseModal={closeModal} mode="dark">
+//           <div>Content Modal</div>
+//         </Modal>
+//       )}
+//       Home
+//       <button type="button" onClick={toggleModal} style={{ width: 40, height: 40, backgroundColor: 'green' }}></button>
+//     </div>
+//   );
+// }
+
+// export default Home;
