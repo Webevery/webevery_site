355994ae8d1893f8d952dd64a4e5c0cab1f58dcd
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2023-05-07 17:39:13 +0300

    add validation form + select

--- src/components/share/Form/Form.jsx
+++ src/components/share/Form/Form.jsx
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Input from '../Input';
-import scss from './Form.module.scss';
+import Textarea from '../Input';
+import styles from './Form.module.scss';
+import Select from './Select';
 
-const Form = props => {
-  const { closeBar, isOpen } = props;
+function Form({ closeBar, isOpen }) {
   const [userName, setUserName] = useState('');
   const [phone, setPhone] = useState('');
   const [mail, setMail] = useState('');
   const [comments, setComments] = useState('');
+  const [selectValue, setSelectValue] = useState('');
+
+  const [dirtyUserName, setDirtyUserName] = useState(false);
+  const [dirtyPhone, setDirtyPhone] = useState(false);
+  const [dirtyMail, setDirtyMail] = useState(false);
+
+  const [errorUserName, setErrorUserName] = useState('Enter name, please');
+  const [errorPhone, setErrorPhone] = useState('Phone can"t be empty');
+  const [errorMail, setErrorMail] = useState('Email can"t be empty');
+  const [errorComments, setErrorComments] = useState('');
+
+  const [validForm, setvalidForm] = useState(false);
+
+  useEffect(() => {
+    if (errorUserName || errorPhone || errorMail || errorComments) {
+      setvalidForm(false);
+    } else {
+      setvalidForm(true);
+    }
+  }, [errorUserName, errorPhone, errorMail, errorComments]);
 
   const formSubmit = evt => {
     evt.preventDefault();
@@ -16,9 +37,15 @@ const Form = props => {
       phone,
       mail,
       comments,
+      type: selectValue,
     };
     console.log('data:', data);
+
     reset();
+  };
+
+  const getSelectValue = evt => {
+    setSelectValue(evt.target.value);
   };
 
   const reset = () => {
@@ -26,25 +53,78 @@ const Form = props => {
     setComments('');
     setPhone('');
     setMail('');
+    setDirtyUserName(false);
+    setDirtyMail(false);
+    setDirtyPhone(false);
+    setErrorUserName('Enter name, please');
+    setErrorPhone('Phone can"t be empty');
+    setErrorMail('Email can"t be empty');
+    setErrorComments('');
+    setSelectValue('');
   };
+
+  const validateName = value => {
+    if (value.length < 2) {
+      setErrorUserName('Name must be longer');
+      if (value.length === 0) {
+        setErrorUserName('Enter name, please');
+      }
+    } else if (value.length > 20) {
+      setErrorUserName('Name must be shorter');
+    } else {
+      setErrorUserName('');
+    }
+  };
+
+  const validateComments = value => {
+    if (value.length > 300) {
+      setErrorComments('Comments must be shorter');
+    } else {
+      setErrorComments('');
+    }
+  };
+
+  function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/;
+
+    if (!re.test(email)) {
+      setErrorMail('Incorrect email');
+    } else {
+      setErrorMail('');
+    }
+  }
+
+  function validatePhone(phone) {
+    let re = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+
+    if (!re.test(phone)) {
+      setErrorPhone('Correct format: (123) 456-7890');
+    } else {
+      setErrorPhone('');
+    }
+  }
 
   const handleChange = evt => {
     const { name, value } = evt.target;
-
+    // console.log('evt.target.name:', evt.target.name);
     switch (name) {
       case 'name':
+        validateName(value);
         setUserName(value);
         break;
 
       case 'comments':
+        validateComments(value);
         setComments(value);
         break;
 
       case 'phone':
+        validatePhone(value);
         setPhone(value);
         break;
 
       case 'mail':
+        validateEmail(value);
         setMail(value);
         break;
 
@@ -53,61 +133,105 @@ const Form = props => {
     }
   };
 
+  const handleBlur = evt => {
+    // console.log('evt.target.name:', evt.target.name);
+    switch (evt.target.name) {
+      case 'name':
+        setDirtyUserName(true);
+        break;
+      case 'mail':
+        setDirtyMail(true);
+        break;
+      case 'phone':
+        setDirtyPhone(true);
+        break;
+
+      default:
+        return;
+    }
+  };
+
   return (
-    <div className={scss.container}>
-      <button type="button" className={scss.closeBtn} onClick={closeBar}>
+    <div className={styles.container}>
+      <button type="button" className={styles.closeBtn} onClick={closeBar}>
         +
       </button>
       <form
         onSubmit={formSubmit}
-        className={isOpen ? scss.form : scss.moveWrap + ' ' + scss.active}
+        className={isOpen ? styles.form : styles.moveWrap + ' ' + styles.active}
       >
-        <div className={scss.wrapGroup}>
-          <Input
-            type="text"
-            id="nameId"
-            name="name"
-            value={userName}
-            label="Name"
-            placeholder=" "
-            onChange={handleChange}
-          />
+        <div className={styles.wrapGroup}>
+          <div className={styles.wrapError}>
+            {dirtyUserName && errorUserName && (
+              <div className={styles.error}>{errorUserName}</div>
+            )}
+            <Input
+              type="text"
+              id="nameId"
+              name="name"
+              value={userName}
+              label="Name"
+              placeholder=" "
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className={styles.wrapError}>
+            {dirtyPhone && errorPhone && (
+              <div className={styles.error}>{errorPhone}</div>
+            )}
+            <Input
+              type="tel"
+              id="phoneId"
+              name="phone"
+              value={phone}
+              label="Phone"
+              placeholder=" "
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
 
-          <Input
-            type="tel"
-            id="phoneId"
-            name="phone"
-            value={phone}
-            label="Phone"
-            placeholder=" "
-            onChange={handleChange}
-          />
-
-          <Input
-            type="email"
-            id="emailId"
-            name="mail"
-            value={mail}
-            label="Email"
-            placeholder=" "
-            onChange={handleChange}
-          />
+          <div className={styles.wrapError}>
+            {dirtyMail && errorMail && (
+              <div className={styles.error}>{errorMail}</div>
+            )}
+            <Input
+              type="email"
+              id="emailId"
+              name="mail"
+              value={mail}
+              label="Email"
+              placeholder=" "
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
         </div>
 
-        <Input
-          type="text"
-          id="commentsId"
-          name="comments"
-          value={comments}
-          label="Comments"
-          placeholder=" "
-          onChange={handleChange}
-        />
+        <Select value={selectValue} getValue={getSelectValue} />
 
-        <button> Send message</button>
+        <div className={styles.wrapError}>
+          {errorComments && <div className={styles.error}>{errorComments}</div>}
+          <Textarea
+            customStyle={styles.textarea}
+            type="text"
+            id="commentsId"
+            name="comments"
+            value={comments}
+            label="Comments"
+            placeholder=" "
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+        </div>
+        <button className={styles.btnSubmit} disabled={!validForm}>
+          {' '}
+          Send message
+        </button>
       </form>
     </div>
   );
-};
+}
 
 export default Form;
