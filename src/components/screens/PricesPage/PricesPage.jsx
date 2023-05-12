401548ebee0e4d3e8aeb15e10ef2c869bf02c3da
Nataliia <natalabojko@gmail.com>
2023-05-12 16:45:54 +0300

    a bicycle from accordion

--- src/components/screens/PricesPage/PricesPage.jsx
+++ src/components/screens/PricesPage/PricesPage.jsx
@@ -1,42 +1,13 @@
 import styles from './PricesPage.module.scss';
 
-import { prices } from 'data/prices';
-
-// import { Link } from 'react-router-dom';
+// import { ourPrices } from './ourPrices';
+import { GetServices } from './GetServices';
 
 const PricesPage = () => {
-  const ourPrices = prices.map(
-    ({ id, title, describton, image, price, details }) => {
-      return (
-        <li key={id}>
-          <div className={styles.textWrapper}>
-            <h3>{title}</h3>
-            <p>{describton}</p>
-
-            {/* <Link className={styles.pricesPage__link} to={'/aboutUs'}>
-            Learn more
-          </Link> */}
-            <button
-              onClick={() => {
-                console.log(id);
-              }}
-            >
-              Learn more
-            </button>
-            <p>
-              Ціна від <span>{price} $</span>
-            </p>
-            <p className={styles.textWrapper__details}>{details}</p>
-          </div>
-          <img src={image} alt={title} />
-        </li>
-      );
-    }
-  );
-
   return (
     <article className={styles.pricesPage}>
-      <ul>{ourPrices}</ul>
+      {/* <ul>{ourPrices}</ul> */}
+      <GetServices />
     </article>
   );
 };
