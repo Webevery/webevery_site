7c116769abd32db02f42e088393e29fbfa008833
Maksimchik36 <Peltek1985@gmail.com>
2023-05-23 15:16:18 +0300

    fixed footer and changed navData and ourTeam

--- src/data/navigation.data.js
+++ src/data/navigation.data.js
@@ -12,26 +12,27 @@ export const navData = [
   {
     id: Math.random(),
     path: '/ourTeam',
-    title: 'OurTeam',
+    title: 'Команда',
   },
-  {
-    id: Math.random(),
-    path: '/contacts',
-    title: 'Contacts',
-  },
-  {
+   {
     id: Math.random(),
     path: '/ourProjects',
-    title: 'OurProjects',
-  },
-  {
-    id: Math.random(),
-    path: '/howWeWork',
-    title: 'How We Work',
+    title: 'Роботи',
   },
   {
     id: Math.random(),
     path: '/prices',
-    title: 'Prices',
+    title: 'Послуги',
+  },
+  {
+    id: Math.random(),
+    path: '/contacts',
+    title: 'Контакти',
+  },
+
+  {
+    id: Math.random(),
+    path: '/howWeWork',
+    title: 'Питання-Вiдповiдь',
   },
 ];
