d1aee2b714fe131c8980de549ccf915282c00201
Maksimchik36 <Peltek1985@gmail.com>
2023-05-27 22:53:54 +0300

    fixed OurTeam page

--- src/components/screens/OurTeamPage/OurTeamPage.jsx
+++ src/components/screens/OurTeamPage/OurTeamPage.jsx
@@ -10,36 +10,66 @@ const OurTeamPage = () => {
 
 
     return (
-        <section className={styles.wrapper}>
-            {!coworkerId ? < img className={styles.mapGeneral} src={mapGeneral} alt="All coworker's locations" /> : <div className={styles.coworker}>
-                <div>
-                    <img className={styles.photo} src={coworkersData[coworkerId].photo} alt={coworkersData[coworkerId].name} />
-                <p>{coworkersData[coworkerId].email}</p>
-                </div>
-                <div className={styles.informationWrapper}>
-                    <p className={styles.name}>{coworkersData[coworkerId].name}</p>
-                    <p className={styles.profession}>{coworkersData[coworkerId].profession}</p>
-                    <p className={styles.city}>{coworkersData[coworkerId].city},
-                        <span className={styles.country} > {coworkersData[coworkerId].country}</span></p>
-                    <p className={styles.quote}>{coworkersData[coworkerId].quote}</p>
-                    <p className={styles.humor}>{coworkersData[coworkerId].humor}</p>
-                    <img className={styles.map} src={coworkersData[coworkerId].location} alt={`location ${coworkersData[coworkerId].name}`}></img>
-                </div>
-            </div>}
+      <section className={styles.wrapper}>
+        {!coworkerId ? (
+          <img
+            className={styles.mapGeneral}
+            src={mapGeneral}
+            alt="All coworker's locations"
+          />
+        ) : (
+          <div className={styles.coworker}>
+              <img
+                className={styles.photo}
+                src={coworkersData[coworkerId].photo}
+                alt={coworkersData[coworkerId].name}
+              />
+            <div className={styles.informationWrapper}>
+              <p className={styles.name}>{coworkersData[coworkerId].name}</p>
+              <p className={styles.profession}>
+                {coworkersData[coworkerId].profession}
+              </p>
+              
+                            
+              <p className={styles.quote}>{coworkersData[coworkerId].quote}</p>
+              <p className={styles.humor}>{coworkersData[coworkerId].humor}</p>
+              <p className={styles.city}>
+                {coworkersData[coworkerId].city},
+                <span className={styles.country}>
+                  {' '}
+                  {coworkersData[coworkerId].country}
+                </span>
+              </p>
+              <img
+                className={styles.map}
+                src={coworkersData[coworkerId].location}
+                alt={`location ${coworkersData[coworkerId].name}`}
+              ></img>
+            </div>
+          </div>
+        )}
 
-            {/* <SimpleCarousel/> */}
+        {/* <SimpleCarousel/> */}
 
-            <ul className={styles.slider}>
-                {coworkersData.map(item => {
-                    return <li className={styles.sliderItem} key={item.id} onClick={() => setCoworkerId(item.id)}>
-                        <img className={styles.sliderPhoto} src={item.photoSmall} alt={item.name}>
-                        </img>
-                    </li>
-                })}
-            </ul>
-
-        </section>
-    )
+        <ul className={styles.slider}>
+          {coworkersData.map(item => {
+            return (
+              <li
+                className={styles.sliderItem}
+                key={item.id}
+                onClick={() => setCoworkerId(item.id)}
+              >
+                <img
+                  className={styles.sliderPhoto}
+                  src={item.photoSmall}
+                  alt={item.name}
+                ></img>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
 }
 
 
