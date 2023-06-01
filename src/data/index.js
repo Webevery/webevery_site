bc2400cd942735a889f1d237d1a678d90845e7f1
Webevery <webforevery@gmail.com>
2023-06-01 17:57:34 +0300

    add_slider_to_projects

--- src/data/index.js
+++ src/data/index.js
@@ -2,3 +2,4 @@ export { navData } from './navigation.data';
 export { socialData } from './social.data';
 export { coworkersData } from './coworkers.data';
 export { pricesData } from './prices.data';
+export { projectsData } from './projects.data';
