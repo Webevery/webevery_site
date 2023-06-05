151672acefaeccce9bd78444163d5276739fadfb
Nataliia <natalabojko@gmail.com>
2023-06-05 17:57:59 +0300

    moved my svg sprite to the general one

--- src/components/screens/ContactsPage/ContactsEmail.jsx
+++ src/components/screens/ContactsPage/ContactsEmail.jsx
@@ -1,11 +1,11 @@
 import styles from './ContactsPage.module.scss';
-import sprite from 'images/contactsImgs/symbol-defs.svg';
+import sprite from 'images/symbol-defs.svg';
 
 export const ContactsEmail = () => {
   return (
     <a href="mailto:natalabojko@gmail.com" className={styles.email}>
       <svg>
-        <use href={sprite + '#icon-mail'} />
+        <use href={sprite + '#icon-contactsMail'} />
       </svg>
       <p> webforevery@gmail.com</p>
     </a>
