951541a69bc3c07dca6681df95a7310a08c43afd
Andrey Tsapkov <tsapkov1994@gmail.com>
2023-06-11 20:29:40 +0300

    faq+welcome

--- src/components/screens/HomePage/HomePage.jsx
+++ src/components/screens/HomePage/HomePage.jsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 // import Globe from 'components/layout/Globe/Globe';
 import Hero from 'components/sections/Hero';
 // import Feedback from 'components/ui/Feedback';
 // import { useModal } from '../../../hooks';
 // import Modal from 'components/share/Modal';
 // import Form from 'components/share/Form';
-import Welcome from 'components/layout/Welcome';
+// import Welcome from 'components/layout/Welcome';
 
 const HomePage = () => {
   // const { isModalOpen, closeModal, toggleModal } = useModal();
-  const [visited, setVisited] = useState(false);
+  // const [visited, setVisited] = useState(false);
 
-  useEffect(() => {
-    sessionStorage.getItem('visited');
+  // useEffect(() => {
+  //   sessionStorage.getItem('visited');
 
-    if (sessionStorage.getItem('visited')) {
-    } else {
-      setVisited(true);
-      const timer = setTimeout(() => {
-        sessionStorage.setItem('visited', 'true');
-        setVisited(false);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }
-  }, [visited]);
+  //   if (sessionStorage.getItem('visited')) {
+  //   } else {
+  //     setVisited(true);
+  //     const timer = setTimeout(() => {
+  //       sessionStorage.setItem('visited', 'true');
+  //       setVisited(false);
+  //     }, 2000);
+  //     return () => clearTimeout(timer);
+  //   }
+  // }, [visited]);
 
   return (
     <>
@@ -35,7 +35,7 @@ const HomePage = () => {
       )} */}
 
       <Hero />
-      {visited && <Welcome />}
+      {/* {visited && <Welcome />} */}
       {/* <Globe /> */}
     </>
   );
