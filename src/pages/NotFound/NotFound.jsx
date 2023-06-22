d34fa8d6dba57fdbb598283b15c77c7a67ed327e
Nataliia <natalabojko@gmail.com>
2023-06-22 18:18:06 +0300

    add Helmet to NotFound page and changed browser logo

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 import NotFoundPage from 'components/screens/NotFoundPage';
 
 const NotFound = () => {
   return (
     <>
+      <Helmet>
+        <title>Як ми робимо веб-сайти</title>
+        <meta
+          name="description"
+          content="Розробка та створення веб-сайтів з індивідуальним дизайном. Сайт-візитка, інтернет-магазин, лендінг, корпоративний сайт. SEO оптимізація."
+        />
+        <link rel="canonical" href="*" />
+      </Helmet>
       <NotFoundPage />
     </>
   );
