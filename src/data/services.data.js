7272af467bc28893d2058138b211e89f4c7fd7a0
Nataliia <natalabojko@gmail.com>
2023-06-28 19:40:46 +0300

    edited OurSrvices according to gesign

--- src/data/services.data.js
+++ src/data/services.data.js
@@ -1,9 +1,11 @@
-import visitSite from 'images/pricesImgs/visitSite2.png';
-import landingSite from 'images/pricesImgs/landingSite.png';
-import corporativeSite from 'images/pricesImgs/corporativeSite.png';
-import shoppingSite from 'images/pricesImgs/shoppingSite.png';
-import catalogSite from 'images/pricesImgs/catalogSite.png';
-import individualProject from 'images/pricesImgs/individualProject.png';
+// import visitSite from 'images/pricesImgs/visitSite2.png';
+// import landingSite from 'images/pricesImgs/landingSite.png';
+// import corporativeSite from 'images/pricesImgs/corporativeSite.png';
+// import shoppingSite from 'images/pricesImgs/shoppingSite.png';
+// import catalogSite from 'images/pricesImgs/catalogSite.png';
+// import individualProject from 'images/pricesImgs/individualProject.png';
+import graphicElements from 'images/services/graphicElements.png';
+import logo from 'images/services/logo.png';
 
 export const servicesData = [
   {
@@ -11,7 +13,7 @@ export const servicesData = [
     title: 'Сайт-візитка',
     description:
       'Простий веб-ресурс, розрахований на 3-5 сторінок і містить: основну інформацію про організацію, приватну особу, послуги, фотогалерею та контактні дані. Сайт-візитка відмінно підійде невеликій компанії, фрілансерам, художникам, приватним викладачам та всім, хто бажає запропонувати низку своїх послуг в інтернеті.',
-    image: visitSite,
+    image: '',
     price: '350',
     daysCount: '7-10 ',
     details: [
@@ -30,7 +32,7 @@ export const servicesData = [
     title: 'Landing Page',
     description:
       'Посадкова сторінка з барвистим дизайном, що продає закликами до дії та зручним розташуванням блоків. Створення односторінкового сайту призначене для запуску реклами, продажу товарів та послуг, збирання контактних даних. Основною перевагою Landing Page є його націленість на потенційного клієнта. Інформація, розміщена на подібному ресурсі, відповідає на всі можливі запитання потенційного замовника товару чи послуги.',
-    image: landingSite,
+    image: '',
     price: '550',
     daysCount: '7-14 ',
     details: [
@@ -54,7 +56,7 @@ export const servicesData = [
     title: 'Сайт каталог',
     description:
       'Основною метою створення сайту каталогу є представлення інформації про компанію, перелік послуг та товарів в електронному вигляді і може складатися з десятків, сотень, а то й тисяч сторінок. Відвідувачі сайту можуть ознайомитися з прайс-листами та описом товарів і послуг, переглянути фотографії та звернутися до компанії для здійснення покупки. Такий сайт може збільшувати кількість продажів за рахунок доступу до повної інформації про продукцію та послуги.',
-    image: catalogSite,
+    image: '',
     price: '1000',
     daysCount: '30-60',
     details: [
@@ -82,7 +84,7 @@ export const servicesData = [
     title: 'Корпоративний сайт',
     description:
       'Офіційний сайт компанії, фірми, закладу чи будь-якого бізнесу. Створення сайту для бізнесу – це найоптимальніший вид сайту для представлення компанії в інтернеті. Він виконує кілька функцій, включаючи: надання послуг; залучення клієнтів; розширення географії продажів; формування позитивного іміджу компанії. Головними перевагами такого сайту є дизайн, контент та функціонал.',
-    image: corporativeSite,
+    image: '',
     price: '1000',
     daysCount: '30-60',
     details: [
@@ -111,7 +113,7 @@ export const servicesData = [
     title: 'Віртуальне меню',
     description:
       'Віртуальне меню  - це зручний спосіб для гостей кафе чи ресторану ознайомитися з асортиментом страв та напоїв безпосередньо на своєму смартфоні. Замість традиційного паперового меню, гості можуть  сканувати спеціальний QR-код за допомогою камери свого телефону або просто відкрити у своєму браузері через звичайний пошуковий рядок.',
-    image: shoppingSite,
+    image: '',
     price: '500',
     daysCount: '30',
     details: [
@@ -139,7 +141,7 @@ export const servicesData = [
     title: 'Індивідуальний проект',
     description:
       'Розробка сайту під ключ із індивідуальними вимогами. В основному всі сайти, створені за персональними проектами замовників, виконують ряд функцій, властивих одразу декількома типами веб-сервісів. Такі проекти здатні підвищити рівень продажів, сформувати позитивний імідж компанії у клієнтів, розширити географію продажів, збирати заявки та особисті дані користувачів тощо.',
-    image: individualProject,
+    image: '',
     price: '500',
     daysCount: '30-60',
     details: [
@@ -161,7 +163,7 @@ export const servicesData = [
     title: 'Тестування',
     description:
       'За допомогою тестування знаходимо помилки, проблеми та дефекти на Вашому сайті. Це включає перевірку реагування на дії користувачів, правильність обробки даних, відображення правильного контенту тощо. Виявлені дефекти можуть бути виправлені, завдяки чому Ваш сайт може працювати безперебійно та задовольняти потреби користувачів.',
-    image: individualProject,
+    image: '',
     price: '300',
     daysCount: '7',
     details: [
@@ -176,24 +178,36 @@ export const servicesData = [
   {
     id: 8,
     title: 'Графічний дизайн',
+
     description:
       'Бріф, дослідження, розробка концепції, створення нарису, робота над вибраними версіями, фінальна прерзентація, затвердження.',
-    image: individualProject,
-    price: '500',
+    image: graphicElements,
+    price: '100',
     daysCount: '7',
-    details: [
-      'Індивідуальний підхід',
-      'Мобільна адаптація	',
-      'Графічні елементи для веб-сторінок або соцмереж',
-      'Логотип',
-      'Ілюстрація',
-    ],
+    details: ['Індивідуальні іконки', 'Патерн', 'Ілюстрація'],
   },
+
   {
     id: 9,
+    title: 'Логотип',
+    description: '',
+    image: logo,
+    price: '100',
+    daysCount: '7',
+    details: [
+      'Логотип, який має ідею',
+      'Індивідуальний підхід',
+      'Унікальний знак',
+      '2-3 варіанти',
+      'Адаптив лого для мобільного телефону, соцмереж',
+    ],
+  },
+
+  {
+    id: 10,
     title: 'Веб дизайн',
     description: '',
-    image: individualProject,
+    image: '',
     price: '500',
     daysCount: '7',
     details: [
