2b692485984a7a3186ea0fd9609e7448f6dfdbdc
Andrey Tsapkov <tsapkov1994@gmail.com>
2023-07-03 19:16:36 +0300

    fix some styles in faq, added mixins

--- src/components/screens/HowWeWorkPage/FAQ/FAQ.jsx
+++ src/components/screens/HowWeWorkPage/FAQ/FAQ.jsx
@@ -21,6 +21,7 @@ const FAQ = () => {
               href={item.href}
               activeIndex={activeIndex}
               setActiveIndex={setActiveIndex}
+              answerHeight={item.answerHeight}
             />
           );
         })}
